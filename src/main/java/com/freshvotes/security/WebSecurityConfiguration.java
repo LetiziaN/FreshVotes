144f1fd3f12321984e170b0b5adf5c725964674a
LetiziaN <letizianaska@gmail.com>
2021-05-02 19:24:11 +0200

    Adding password encoder

--- src/main/java/com/freshvotes/security/WebSecurityConfiguration.java
+++ src/main/java/com/freshvotes/security/WebSecurityConfiguration.java
@@ -1,22 +1,33 @@
 package com.freshvotes.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 
     @Override
     protected void configure (AuthenticationManagerBuilder auth) throws Exception{
 
         auth.inMemoryAuthentication()
-        .withUser("letizianaska@gmail.com")
-        .password("123Leti123")
-        .roles("USER");
+            .passwordEncoder(getPasswordEncoder())
+            .withUser("letizianaska@gmail.com")
+            .password(getPasswordEncoder().encode("123Leti123"))
+            .roles("USER");
 
     }
+
+    
 
     @Override
     protected void configure(HttpSecurity http) throws Exception{
